78cd9fa8a3dbc944d3749d5f46d36b84d0f511b4
mimi222824 <mahboub.oumaima2022@gmail.com>
2025-11-23 22:21:07 +0100

    initial commit

--- main.java
+++ main.java
@@ -1,3 +1,19 @@
-public class main {
-    
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Entrez un nombre : ");
+        int n = scanner.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println(n + " est pair.");
+        } else {
+            System.out.println(n + " est impair.");
+        }
+
+        scanner.close();
+    }
 }
